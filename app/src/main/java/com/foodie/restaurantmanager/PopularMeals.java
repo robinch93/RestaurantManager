f6dc02cbe106566d5ad5ae190a3acfdb64335726
Robin Chaudhary <torino2019>
2019-06-10 21:15:49 +0200

    image button

--- app/src/main/java/com/foodie/restaurantmanager/PopularMeals.java
+++ app/src/main/java/com/foodie/restaurantmanager/PopularMeals.java
@@ -2,6 +2,8 @@ package com.foodie.restaurantmanager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Hema Bhandari on 5/25/2019.
@@ -14,5 +16,12 @@ public class PopularMeals extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popular_meals);
+        ImageButton backButton = (ImageButton)this.findViewById(R.id.btnBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
